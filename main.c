098bf32f272abd40a919a61671a7367453541db8
carlos.guaita <carlos.guaita@udla.edu.ec>
2023-05-02 15:32:44 -0500

    inicio

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+    float x1 = ingresarCoordenadaX();
+    float y1 = ingresarCoordenadaY();
+    float x2 = ingresarCoordenadaX();
+    float y2 = ingresarCoordenadaY();
+    float x3 = ingresarCoordenadaX();
+    float y3 = ingresarCoordenadaY();
+
+    float radio = obtenerRadio(x1,y1,x2,y2,x3,y3);
+
+    obtenerArea(radio);
+
+
+    return 0;
+}
